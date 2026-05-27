d54c8cc839006490631ce40ff5d447a52930ca7e
NickyHariniai <ferrissushi@gmail.com>
2026-05-27 11:06:25 +0300

    chore(service): add arithmetic service

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/arithmetic/AddController.java
@@ -0,0 +1,17 @@
+package com.example.demo.endpoint.rest.controller.arithmetic;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/add")
+public class AddController {
+    // @GetMapping
+    // public ResponseEntity<?> add(
+    //         @RequestParam int a,
+    //         @RequestParam int b) {
+    // }
+}
