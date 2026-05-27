ad571bc014b60e9b63512962dfa843a093a319ef
NickyHariniai <ferrissushi@gmail.com>
2026-05-27 11:28:18 +0300

    style: format

--- src/main/java/com/example/demo/service/ArithmeticService.java
+++ src/main/java/com/example/demo/service/ArithmeticService.java
@@ -4,35 +4,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ArithmeticService {
-    public int add(int a, int b) {
-        if (a < 0 || b < 0) {
-            throw new IllegalArgumentException("a or b cannot be negative");
-        }
-        return a + b;
+  public int add(int a, int b) {
+    if (a < 0 || b < 0) {
+      throw new IllegalArgumentException("a or b cannot be negative");
     }
+    return a + b;
+  }
 
-    public int subtract(int a, int b) {
-        if (a < 0 || b < 0) {
-            throw new IllegalArgumentException("a or b cannot be negative");
-        }
-        return a - b;
+  public int subtract(int a, int b) {
+    if (a < 0 || b < 0) {
+      throw new IllegalArgumentException("a or b cannot be negative");
     }
+    return a - b;
+  }
 
-    public int multiply(int a, int b) {
-        if (a < 0 || b < 0) {
-            throw new IllegalArgumentException("a or b cannot be negative");
-        }
-        return a * b;
+  public int multiply(int a, int b) {
+    if (a < 0 || b < 0) {
+      throw new IllegalArgumentException("a or b cannot be negative");
     }
+    return a * b;
+  }
 
-    public int divide(int a, int b) {
-        if (a < 0 || b <= 0) {
-             throw new IllegalArgumentException("a or b cannot be negative");
-        }
-        if (b == 0) {
-            throw new IllegalArgumentException("b cannot be 0");
-        }
-        return a / b;
+  public int divide(int a, int b) {
+    if (a < 0 || b <= 0) {
+      throw new IllegalArgumentException("a or b cannot be negative");
     }
-
+    if (b == 0) {
+      throw new IllegalArgumentException("b cannot be 0");
+    }
+    return a / b;
+  }
 }
